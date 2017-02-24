381b5fbcc7cf42298b5d06cc1cabcfe942569be1
Elif Benli <elfbnli@gmail.com>
2017-02-25 00:01:14 +0300

    Local database'e bağlanması sağlandı. Database'e eleman eklenmesi yapıldı.

--- NeYesekApp/Default.aspx.cs
+++ NeYesekApp/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NeYesekApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,18 @@ namespace NeYesekApp
 {
     public partial class Default : System.Web.UI.Page
     {
+        private NeYesekAppContext db = new NeYesekAppContext();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            db.Database.CreateIfNotExists();
+            GroupMember member = db.GroupMembers.Add(new GroupMember()
+            {
+                Id = 6,
+                Name = "Hasan Akhasan",
+                Category = "AKP",
+                Price = 3.5
+            });
+            db.SaveChanges();
         }
     }
 }
---
+++ NeYesekApp/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace NeYesekApp.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NeYesekApp.Models.NeYesekAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(NeYesekApp.Models.NeYesekAppContext context)
+        {
+            context.GroupMembers.AddOrUpdate(x => x.Id,
+                new Models.GroupMember(5, "Elif Benli", "Owner", 4.5),
+                new Models.GroupMember(5, "İlkan Engin", "Owner", 1.5),
+                new Models.GroupMember(5, "Rıdvan Sırma", "Owner", 6.5));
+            context.SaveChanges();
+        }
+    }
+}

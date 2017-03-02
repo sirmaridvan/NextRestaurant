97f418e8aed4d1f5f4f3d395768bd347e25b72aa
Elif Benli <elfbnli@gmail.com>
2017-03-02 20:01:21 +0300

    Restaurant, User, UserVotes table ları yaratıldı. İlgili relationship ler konuldu.

--- NeYesekApp/Default.aspx.cs
+++ NeYesekApp/Default.aspx.cs
@@ -10,17 +10,30 @@ namespace NeYesekApp
 {
     public partial class Default : System.Web.UI.Page
     {
-        private NeYesekAppContext db = new NeYesekAppContext();
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            GroupMember member = db.GroupMembers.Add(new GroupMember()
-            {
-                Id = 6,
-                Name = "Hasan Akhasan",
-                Category = "AKP",
-                Price = 3.5
-            });
-            db.SaveChanges();
+            using (var db = new NeYesekAppContext()) {
+
+                var user = (from d in db.Users where d.Name == "Elif Benli" select d).Single();
+
+                var restaurant = (from d in db.Restaurants where d.Name == "Sampi" select d).Single();
+
+                db.UserVotes.Add(new UserVote()
+                {
+                    UserId = user.Id,
+                    RestaurantId = restaurant.Id,
+                    Vote = 6.5
+                });
+
+                db.SaveChanges();
+
+                foreach (var vote in user.Votes)
+                {
+                    
+                }
+
+            }
         }
     }
 }
---
+++ NeYesekApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NeYesekApp.Models
+{
+    public class User
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+        [Required, MaxLength(30)]
+        public string Email { get; set; }
+        [Required]
+        public string Salt { get; set; }
+        [Required]
+        public string Hash { get; set; }
+
+        [InverseProperty("User")]
+        public virtual ICollection<UserVote> Votes { get; set; }
+    }
+}

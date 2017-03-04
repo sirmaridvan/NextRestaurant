1c88c00d517c7e524b2b1ed03975be1ad32e63c3
Elif Benli <elfbnli@gmail.com>
2017-03-04 20:08:11 +0300

    Login ve register sayfaları tamamlandı.

---
+++ NeYesekApp/General.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace NeYesekApp
+{
+    public class General
+    {
+
+        public static byte[] GetHash(string inputString)
+        {
+            HashAlgorithm algorithm = MD5.Create();  //or use SHA1.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+    }
+}
